a2c83b66b002201467e6c103b4120f40abaa72b7
HopeOwl <64351667+HopeOwl@users.noreply.github.com>
2022-03-01 03:09:32 -0300

    Evitando aparecer erro
    Agora quando rodar o script, o erro 'find_element_by_* commands are deprecated in selenium', não aparece mais.

--- PesquisaAutomatica.py
+++ PesquisaAutomatica.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.common import by
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 item = input('qual item? ')
@@ -7,4 +7,4 @@ pesquisa = str(f'{item} para que serve?' )
 
 driver = webdriver.Chrome()
 driver.get('https://www.google.com.br')
-driver.find_element_by_name('q').send_keys(pesquisa + Keys.RETURN)
+driver.find_element(By.NAME, 'q').send_keys(pesquisa + Keys.RETURN)
